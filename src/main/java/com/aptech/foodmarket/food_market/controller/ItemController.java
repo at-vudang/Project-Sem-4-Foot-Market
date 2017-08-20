c3da59a3afd3aade6fd9d4f55926ceb0f1210eb7
at-VanTu <letu710@gmail.com>
2017-08-20 22:08:46 +0700

    update 20-08-2017

--- src/main/java/com/aptech/foodmarket/food_market/controller/ItemController.java
+++ src/main/java/com/aptech/foodmarket/food_market/controller/ItemController.java
@@ -1,9 +1,36 @@
 package com.aptech.foodmarket.food_market.controller;
 
+import com.aptech.foodmarket.food_market.model.Item;
+import com.aptech.foodmarket.food_market.model.User;
 import com.aptech.foodmarket.food_market.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@Controller
 public class ItemController {
     @Autowired
     ItemRepository itemRepository ;
+
+    @RequestMapping("/all")
+    @ResponseBody
+    public List<Item> getAll() {
+             return itemRepository.findAll();
+    }
+
+    @RequestMapping("/getItemByName")
+    @ResponseBody
+    public List<Item> getItemByName(String name) {
+        System.out.println(name);
+        return itemRepository.findByName(name);
+    }
+
+    @RequestMapping("/getItemById")
+    @ResponseBody
+    public Item getItemByName(int id) {
+        return itemRepository.findOne(id);
+    }
 }
--- src/main/java/com/aptech/foodmarket/food_market/service/ItemService.java
+++ src/main/java/com/aptech/foodmarket/food_market/service/ItemService.java
@@ -3,18 +3,24 @@ package com.aptech.foodmarket.food_market.service;
 import com.aptech.foodmarket.food_market.model.Item;
 import com.aptech.foodmarket.food_market.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Service
 public class ItemService {
     @PersistenceContext
     private EntityManager entityManager;
     @Autowired
     private ItemRepository itemRepository;
 
-    public List<Item> getAll() {
-        return itemRepository.findAll();
+//    public List<Item> getAll() {
+//        return itemRepository.findAll();
+//    }
+
+    public List<Item> getItemByName(String name) {
+        return itemRepository.findByName(name);
     }
 }
