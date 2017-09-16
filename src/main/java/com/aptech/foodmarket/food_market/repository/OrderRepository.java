48321ec8717a2e419757e0a23d5ed922d8eb01b4
at-VanTu <letu710@gmail.com>
2017-09-16 21:25:32 +0700

    api get order by user

--- src/main/java/com/aptech/foodmarket/food_market/repository/OrderRepository.java
+++ src/main/java/com/aptech/foodmarket/food_market/repository/OrderRepository.java
@@ -2,8 +2,13 @@ package com.aptech.foodmarket.food_market.repository;
 
 import com.aptech.foodmarket.food_market.model.Category;
 import com.aptech.foodmarket.food_market.model.Order;
+import com.aptech.foodmarket.food_market.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Integer>{
-    
+        Page<Order> findAllByUser(User user, Pageable pageable);
 }
